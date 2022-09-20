9a77ed834dff80b5e0268f232defa36ae7a9a4c9
Shama Parveen <77065652+Shama-dotcom@users.noreply.github.com>
2022-09-20 11:26:17 +0530

    Update Card.js

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,27 +1,82 @@
 import React, { useState } from 'react';
 import { FaCartPlus } from 'react-icons/fa';
-import './Card.css';
 
+
+import './Card.css';
 
 const Cards = ({ item, handleClick }) => {
   const { title, price, description, image, category, rating, size, stock } =
     item;
 
-    // const [data, setData] = useState(list);
-    // const filterResult = (item) => {
-    //     const result= list.filter((curData) => {
-    //         return curData.size === item;
-    //     });
-    //     setData(result);
-    // }
+    const [quantity, setQuantity] = useState(0);
+
+    const handleDecrement = () => {
+      setQuantity(quantity - 1);
+      if (quantity<= 0) {
+        alert('Select atleast one')
+    }
+      console.log('clicked');
+    };
+  
+    const handleIncrement = () => {
+      setQuantity(quantity + 1);
+      console.log('clicked');
+    };
+  
+  // const [data, setData] = useState(list);
+  // const filterResult = (item) => {
+  //     const result= list.filter((curData) => {
+  //         return curData.size === item;
+  //     });
+  //     setData(result);
+  // }
+  // const [localProducts, setLocalProducts] = useState([]);
+  // const [searchTerm, setSearchTerm] = useState('');
+
+  // useEffect(() => {
+  //   setLocalProducts(productList);
+  // }, [productList]);
 
 
+  // useEffect(() => {
+  //   // search item
+  //   if (searchTerm.length > 0) {
+  //     setLocalProducts(
+  //       productList?.filter((item) => item?.title.includes(searchTerm))
+  //     );
+  //   } else {
+  //     setLocalProducts(productList);
+  //   }
+  // }, [searchTerm]);
 
+  // const onFilterValueChanged = (val) => {
+  //   console.log('val', val);
+  //   if (val) {
+  //     setLocalProducts(productList?.filter((item) => item?.size === val));
+  //   }
+  // };
+
+  // const onFilterCategoryChanged = (val) => {
+  //   console.log('val', val);
+  //   if (val) {
+  //     setLocalProducts(
+  //       productList?.filter((item) => item?.category?.toLowerCase() === val)
+  //     );
+  //   }
+  // };
+
+  // console.log('searchTerm', searchTerm);
   return (
     <>
-      
+    
+{/* 
+<Header
+            searchTerm={searchTerm}
+            setSearchTerm={setSearchTerm}
+            onFilterValueChanged={onFilterValueChanged}
+            onFilterCategoryChanged={onFilterCategoryChanged}
+          /> */}
       <div className="cards">
-      
         <div className="image_box">
           {' '}
           <img src={image} alt="" />
@@ -30,6 +85,14 @@ const Cards = ({ item, handleClick }) => {
             <span className="title">{title}</span>
           </h5>
           <h5>Price: {price} $</h5>
+          <h5>
+            Quantity:{' '}
+            <div>
+              <button onClick={handleDecrement}>-</button>
+              <button>{quantity}</button>
+              <button onClick={handleIncrement}>+</button>
+            </div>
+          </h5>
           <button onClick={() => handleClick(item)}>
             Add to Cart <FaCartPlus />
           </button>
